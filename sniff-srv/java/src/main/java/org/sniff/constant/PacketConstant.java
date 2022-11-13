f3df9923775d65f20bcc5575d56877d807e86c5e
Tokhta Horqin <tokhta@outlook.com>
2022-11-13 22:28:20 +0800

    删去 constant 部分代码

--- sniff-srv/java/src/main/java/org/sniff/constant/PacketConstant.java
+++
@@ -1,7 +0,0 @@
-package org.sniff.constant;
-
-public interface PacketConstant {
-
-    Long minCount = 3L;
-    Long maxCount = 24L;
-}
--- sniff-srv/java/src/main/java/org/sniff/service/impl/SplitServiceImpl.java
+++ sniff-srv/java/src/main/java/org/sniff/service/impl/SplitServiceImpl.java
@@ -6,7 +6,6 @@ import io.pkts.packet.IPv4Packet;
 import io.pkts.packet.IPv6Packet;
 import io.pkts.packet.TransportPacket;
 import io.pkts.protocol.Protocol;
-import org.sniff.constant.PacketConstant;
 import org.sniff.entity.Session;
 import org.sniff.service.SplitService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -23,6 +22,8 @@ import java.nio.file.Path;
 
 @Service
 public class SplitServiceImpl implements SplitService {
+
+    private final Long MAX_COUNT = 24L;
 
     @Value("${config.delay-time}")
     private Long delayTime;
@@ -68,7 +69,7 @@ public class SplitServiceImpl implements SplitService {
                         m.getMessageProperties().getHeaders().put("x-delay", delayTime);
                         return m;
                     });
-                } else if (count > PacketConstant.maxCount
+                } else if (count > MAX_COUNT
                         && !stringRedisTemplate.opsForSet().isMember("done-session-entry", key)) {
                     // 如果采集的数据包数量足够，并且没有受到处理，直接添加消息队列
                     // 特殊情况：done-session-entry 集合还未创建，此时当作尚未进行分析

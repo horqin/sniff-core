fd879fdc7e3ed081fe0dcba8c19d447e82a9e4f7
Tokhta Horqin <tokhta@outlook.com>
2022-11-18 18:45:43 +0800

    关闭 ZK 输出

--- sniff-cli/main.c
+++ sniff-cli/main.c
@@ -33,6 +33,8 @@ static char server[64]; // 服务器的地址
 static void watcher(zhandle_t *zh, int type, int stat, const char *path, void *ctx);
 
 int main(int argc, const char *argv[]) {
+    zoo_set_debug_level(0);
+
     // 获取 ZK 数据库的地址和路径
     char *host, *path;
     if ((host = getenv("ZK_HOST")) == NULL || (path = getenv("ZK_PATH")) == NULL) {

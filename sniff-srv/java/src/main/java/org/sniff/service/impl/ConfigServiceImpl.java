db25172ebe31fa7940aa711b534a9ff70db58b01
Tokhta Horqin <tokhta@outlook.com>
2022-12-17 23:11:05 +0800

    在启动时，同步 MySQL 中的数据到 ZK 中

--- sniff-srv/java/src/main/java/org/sniff/service/impl/ConfigServiceImpl.java
+++ sniff-srv/java/src/main/java/org/sniff/service/impl/ConfigServiceImpl.java
@@ -1,12 +1,15 @@
 package org.sniff.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import org.apache.curator.framework.CuratorFramework;
+import org.sniff.mapper.ConfigMapper;
 import org.sniff.pojo.Config;
 import org.sniff.service.ConfigService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -15,6 +18,19 @@ public class ConfigServiceImpl implements ConfigService {
 
     @Resource
     private CuratorFramework curatorFramework;
+
+    @Resource
+    private ConfigMapper configMapper;
+
+    @PostConstruct
+    private void synchronizing() throws Exception {
+        List<Config> configs = configMapper.selectList(new QueryWrapper<>());
+        for (Config config : configs) {
+            String path = "/config/" + config.getName();
+            curatorFramework.create().creatingParentsIfNeeded().forPath(path,
+                    new Gson().toJson(config).getBytes());
+        }
+    }
 
     @Override
     public void config(String type, List<Config> configs) throws Exception {

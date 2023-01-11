931992a7f70288595549775ca0b484d798393953
Tokhta Horqin <tokhta@outlook.com>
2023-01-11 13:18:31 +0800

    使用PathVariable替代Argument

--- sniff-srv/java/src/main/java/org/sniff/controller/SplitCapController.java
+++ sniff-srv/java/src/main/java/org/sniff/controller/SplitCapController.java
@@ -2,10 +2,7 @@ package org.sniff.controller;
 
 import org.sniff.service.SplitCapService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -18,8 +15,8 @@ public class SplitCapController {
     private SplitCapService splitCapService;
 
     // 嗅探服务器
-    @PostMapping(consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public void split(HttpServletRequest request, @RequestParam String file) {
+    @PostMapping(value = "/{file}", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void split(@PathVariable("file") String file, HttpServletRequest request) {
         try {
             splitCapService.splitCap(request.getInputStream());
         } catch (Exception e) {
